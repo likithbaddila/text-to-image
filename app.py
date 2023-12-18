c7f5d3157ddae6a03e2c5539f921868cffe5bbe3
likithbaddila <baddilalikhith@gmail.com>
2023-12-19 00:14:21 +0530

    updated api key

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import openai
 import os
 
 from openai import OpenAI
-client = OpenAI(api_key=os.environ.get("api_key"))
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
 
 def generate_image(prompt, image_size):
     response = client.images.generate(
